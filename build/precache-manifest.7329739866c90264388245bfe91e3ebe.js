8bae302958416231d711e0c54ff7ee230cbcbb8c
Sezayi Arslan <arslan.sezayi@gmail.com>
2020-07-23 12:24:05 +0200

    commit all

---
+++ build/precache-manifest.7329739866c90264388245bfe91e3ebe.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf16e0a1134500ff10a07ec83c0e613c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fa578b4265dbfef4bf1",
+    "url": "/static/css/2.ea08798d.chunk.css"
+  },
+  {
+    "revision": "d77f78a51ef2f91dbcb3",
+    "url": "/static/css/main.b05adfac.chunk.css"
+  },
+  {
+    "revision": "7fa578b4265dbfef4bf1",
+    "url": "/static/js/2.0e38fa9d.chunk.js"
+  },
+  {
+    "revision": "d77f78a51ef2f91dbcb3",
+    "url": "/static/js/main.7d8de10e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
